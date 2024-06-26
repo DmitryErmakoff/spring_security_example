2e1c2d0ff0691c8cd60e1345bcbac58511070322
DmitryErmakoff <dimka6466erm@gmail.com>
2024-06-26 16:20:30 +0500

    init project

---
+++ src/main/java/ru/d3m4k/spring/security_jwt_example/SecurityJwtExampleApplication.java
@@ -0,0 +1,13 @@
+package ru.d3m4k.spring.security_jwt_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/d3m4k/spring/security_jwt_example/SecurityJwtExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.d3m4k.spring.security_jwt_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
